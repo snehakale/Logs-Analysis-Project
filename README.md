cbcd7270755c962afd00ccc9dd5111f3be0d8724
Sneha Kale <kalesneha88@gmail.com>
2017-10-11 13:15:19 -0400

    After first review
    Changes in queries.py and README.md file after first review

--- README.md
+++ README.md
@@ -8,27 +8,38 @@ This is Logs Analysis project which builds an internal reporting tool holding an
     1) [python 3](https://www.python.org/downloads/)
     2) [Vagrant](https://www.vagrantup.com/) and [VirtualBox](https://www.virtualbox.org/wiki/Downloads) to install and manage the VM
    3) [Python3 Psycopg2 DB-API](http://initd.org/psycopg/docs/install.html)
+    4) [Download the folder](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip) for the database operations.
 
 ## Installation 
 1. Download the given project folder.
-2. Install Vagrant and VirtualBox. This will give you the PostgreSQL database and support software needed for this project.
+2. Install Vagrant and VirtualBox. 
+This will give you the PostgreSQL database and support software needed for this project.
 3. You can use Github to fork and clone the repository [here](https://github.com/udacity/fullstack-nanodegree-vm) for VM configuration. 
 4. From your terminal, inside the vagrant subdirectory, run the command **vagrant up**. This will cause Vagrant to download the Linux operating system and install it.
 5. When vagrant up is finished running, you will get your shell prompt back. At this point, you can run **vagrant ssh** to log in to your newly installed Linux VM!
-6. Download and install [Python3 Psycopg2 DB-API](http://initd.org/psycopg/docs/install.html)
-7. Run the python file viz. `queries.py` from the terminal to get the project results. 
+6. Unzip the downloaded database folder. There is a file inside called as `newsdata.sql`. Put this file into the vagrant directory, which is shared with your virtual machine. To load the data, cd into the **vagrant** directory and use the command 
+`psql -d news -f newsdata.sql`.
+Running this command will connect to your installed database server and execute the SQL commands in the downloaded file, creating tables and populating them with data.
+7. Download and install [Python3 Psycopg2 DB-API](http://initd.org/psycopg/docs/install.html)
+8. From the downloaded project folder, get the python file `queries.py` and place it in **vagrant** directory,which is shared with your virtual machine. cd into **vagrant** directory and run the python file viz. `queries.py` from the terminal.
+eg. `vagrant@vagrant:/vagrant$ python queries.py`
+
 
 ## Code Example 
 This project contains one python file viz. `queries.py`
+1) It has shebang line which provides the version of python required for the interpreter.
+`#! /usr/bin/env python3`
 1) It imports `pysocpg2` python DB-API 
-2) There is a code to make a connection with the given database.
-` conn = psycopg2.connect("dbname=news")`
-3) It creates queries to fetch the information from connected database.
-eg. `query1 = ("select a.title , count(l.path) from articles as a, log as l "....`
-4) It uses `.format()` python functionality to format the output data.
-eg. `print('{} - {} views'.format(row[0], row[1]))`
-5) At the end it closes the connection made with the database.
-`conn.close()`
+2) Function `db_connect(...)` creates a connection with the given database and returns the connected database object and cursor for it.
+`db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor`
+3) Function `get_result(..)` will execute the queries written inside it
+eg. `query1 = ("select a.title , count(l.path) from articles as a,...`
+ and uses  `.format()` python functionality to format the output data.
+eg. `print ('{} - {} views'.format(row[0], row[1]))`
+ At the end it closes the connection made with the database.
+`db.close()`
 
 ## References 
 1. [Python psycopg2 DB-API](http://initd.org/psycopg/docs/)
--- queries.py
+++ queries.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 ''' This file is a python program (version 3.6.2)
     which makes a connection with the database,
     creates and executes postgresql queries to get the logs information.
@@ -6,55 +7,69 @@
 # importing python DB-API
 import psycopg2
 
-# Connecting to the database
-conn = psycopg2.connect("dbname=news")
-# Getting the cursor for query execution
-c = conn.cursor()
 
-# To get the most popular 3 articles of all time
-query1 = ("select a.title , count(l.path) from articles as a, log as l "
-          "where '/article/'||a.slug like l.path "
-          "group by a.title order by count(l.path) desc limit 3;")
-c.execute(query1)
-rows1 = c.fetchall()
+# function to make a connection with the database and to get the cursor
+def db_connect(database_name):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Error while connecting to the database.")
 
-# printing the result
-print(" ")
-print("The most popular three articles of all time are :")
-for row in rows1:
-    print('{} - {} views'.format(row[0], row[1]))
 
-# To get most popular article authors of all time
-query2 = ("select t.name, count(l.path) from authors as t "
-          "join articles as a on t.id = a.author "
-          "join log as l on '/article/'||a.slug = l.path "
-          "group by t.name order by count(l.path) desc;")
-c.execute(query2)
-rows2 = c.fetchall()
+# function to execute the queries and to get the results
+def get_result():
+    db, cursor = db_connect("news")
 
-# printing the result
-print(" ")
-print("The most popular article authors of all time are :")
-for row in rows2:
-    print('{} - {} views'.format(row[0], row[1]))
+    # To get the most popular 3 articles of all time
+    query1 = ("select a.title , count(l.path) from articles as a, log as l "
+              "where '/article/'||a.slug like l.path "
+              "group by a.title order by count(l.path) desc limit 3;")
+    cursor.execute(query1)
+    rows1 = cursor.fetchall()
 
-# To get on which days did more than 1% of requests lead to errors
-query3 = ("select l1.date1, ((errors*100)/(success+errors))::float "
-          "as percentage "
-          "from (select date(time) as date1, count(*) as success "
-          "from log where status like '%200%' group by date(time)) as l1 "
-          "join (select date(time) as date2, count(*) as errors "
-          "from log where status like '%404%' group by date(time)) as l2 "
-          "on l1.date1 = l2.date2 "
-          "where ((errors*100)/(success + errors))::float > (0.01)::float;")
-c.execute(query3)
-rows3 = c.fetchall()
+    # printing the result
+    print(" ")
+    print("The most popular three articles of all time are :")
+    for row in rows1:
+        print('{} - {} views'.format(row[0], row[1]))
 
-# printing the result
-print(" ")
-print("Days which did more than 1% of requests lead to errors are :")
-for row in rows3:
-    print('{:%m/%d/%y} - {} %'.format(row[0], row[1]))
+    # To get most popular article authors of all time
+    query2 = ("select t.name, count(l.path) from authors as t "
+              "join articles as a on t.id = a.author "
+              "join log as l on '/article/'||a.slug = l.path "
+              "group by t.name order by count(l.path) desc;")
+    cursor.execute(query2)
+    rows2 = cursor.fetchall()
 
-# closing the connection with databse
-conn.close()
+    # printing the result
+    print(" ")
+    print("The most popular article authors of all time are :")
+    for row in rows2:
+        print('{} - {} views'.format(row[0], row[1]))
+
+    # To get on which days did more than 1% of requests lead to errors
+    query3 = ("select l1.date1, ((errors*100)/(success+errors))::float "
+              "as percentage "
+              "from (select date(time) as date1, count(*) as success "
+              "from log where status like '%200%' group by date(time)) as l1 "
+              "join (select date(time) as date2, count(*) as errors "
+              "from log where status like '%404%' group by date(time)) as l2 "
+              "on l1.date1 = l2.date2 "
+              "where ((errors*100)/(success + errors))::float "
+              "> (0.01)::float;")
+    cursor.execute(query3)
+    rows3 = cursor.fetchall()
+
+    # printing the result
+    print(" ")
+    print("Days which did more than 1% of requests lead to errors are :")
+    for row in rows3:
+        print('{:%m/%d/%y} - {} %'.format(row[0], row[1]))
+
+    # closing the connection with databse
+    db.close()
+
+# Calling function get_result to get the output
+get_result()
